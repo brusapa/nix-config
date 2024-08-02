73e6c08b9f659fc34d654ab21b0391d299c7e6e8
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2024-08-02 15:22:10 +0200

    Use yubikey-touch-detector

--- modules/hardware/yubikey.nix
+++ modules/hardware/yubikey.nix
@@ -4,4 +4,22 @@
 
   services.udev.packages = [ pkgs.yubikey-personalization ];
 
+  environment.systemPackages = with pkgs; [
+    yubikey-touch-detector
+  ];
+
+  # Get a notification when the yubikey is waiting for a touch
+  systemd.user.services.yubikey-touch-detector = {
+    description = "Yubikey touch detector";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.yubikey-touch-detector}/bin/yubikey-touch-detector --libnotify";
+    };
+  };
+
+  # Allow use of sudo with yubikey
+  security.pam.services = {
+    sudo.u2fAuth = true;
+  };
+
 }
