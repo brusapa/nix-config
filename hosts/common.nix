6eea95e8a7cf9b46733311740660b8c7083e2763
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2024-08-05 17:37:24 +0200

    Allow SSH agent forwarding.
    Allow sudo usage through SSH agent

--- hosts/common.nix
+++ hosts/common.nix
@@ -50,5 +50,13 @@
       PasswordAuthentication = false;
       AllowUsers = [ "bruno" ];
     };
+    extraConfig = ''
+      AllowAgentForwarding yes
+      StreamLocalBindUnlink yes
+    '';
   };
+
+  # Allow sudo through ssh agent
+  security.pam.sshAgentAuth.enable = true;
+  security.pam.services.sudo.sshAgentAuth = true;
 }
