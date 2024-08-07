da71b35b7ff989172c5c902a8c637a791c76a9eb
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2024-08-07 08:12:16 +0200

    Remove virtualbox (for the time being at least)

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -3,9 +3,9 @@
 {
 
   # Virtualbox
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  users.extraGroups.vboxusers.members = [ "bruno" ];
+  # virtualisation.virtualbox.host.enable = true;
+  # virtualisation.virtualbox.host.enableExtensionPack = true;
+  # users.extraGroups.vboxusers.members = [ "bruno" ];
   
   # Docker
   virtualisation.docker.enable = true;
