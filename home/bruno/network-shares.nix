2120bcf741ec92df594964e9013c0bb8e89415e0
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2024-09-11 10:05:58 +0200

    Add network share for casa for Bruno user

--- home/bruno/network-shares.nix
+++ home/bruno/network-shares.nix
@@ -17,4 +17,15 @@
     in ["${automount_opts},credentials=${config.users.users.bruno.home}/.smb-secrets,uid=${toString config.users.users.bruno.uid},gid=${toString config.users.groups.users.gid}"];
   };
 
+  # SMB NAS Casa Bruno
+  fileSystems."/home/bruno/NAS/casa" = {
+    device = "//nas.rex-eagle.ts.net/casa";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=${config.users.users.bruno.home}/.smb-secrets,uid=${toString config.users.users.bruno.uid},gid=${toString config.users.groups.users.gid}"];
+  };
+
 }
