76ca2399c09e3130172ae2557b09d915ba9e575c
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2024-09-20 15:19:15 +0200

    System upgrade

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -28,38 +28,7 @@
     enable = true;
     autoStart = true;
     capSysAdmin = true;
-    openFirewall = true;  
-    # settings = {
-    #   global_prep_cmd = "[{\"do\":\"sh -c \"${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-3.mode.\${SUNSHINE_CLIENT_WIDTH}x\${SUNSHINE_CLIENT_HEIGHT}@\${SUNSHINE_CLIENT_FPS}\"\",\"undo\":\"${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-3.mode.5120x1440@240\"}]";
-    # };
-    # applications = {
-    #   env = {
-    #     PATH = "$(PATH):$(HOME)/.local/bin";
-    #   };
-    #   apps = [
-    #     {
-    #       name = "Desktop";
-    #       image-path = "desktop.png";
-    #     }
-    #     {
-    #       name = "Low Res Desktop";
-    #       image-path = "desktop.png";
-    #       prep-cmd = [
-    #         {
-    #           do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-3.mode.1920x1080";
-    #           undo = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-3.mode.5120x1440@240";
-    #         }
-    #       ];
-    #     }
-    #     {
-    #       name = "Steam Big Picture";
-    #       detached = [
-    #         "${pkgs.util-linux}/bin/setsid ${pkgs.steam}/bin/steam steam://open/bigpicture"
-    #       ];
-    #       image-path = "steam.png";
-    #     }
-    #   ];
-    # };
+    openFirewall = true;
   };
 
 }
