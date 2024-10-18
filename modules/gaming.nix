79b233c14df6ec5c5cef9cbaaf8f72fb9fc0614a
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2024-10-18 08:29:44 +0200

    Fix gamescope

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -10,15 +10,32 @@
   ];
 
   programs.gamemode.enable = true;
+  users.groups.gamemode.members = [ "bruno" ];
 
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    package = pkgs.steam.override { # Fix gamescope not working on steam due to missing libraries https://github.com/NixOS/nixpkgs/issues/214275
+      extraPkgs = pkgs:
+        with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
+    };
   };
 
   # Modify the game launcher to use the following options
+  # gamescope --force-grab-cursor -W 2560 -H 1440 -r 120 -f -e gamemoderun %command%
   # gamemoderun %command%
   # mangohud %command%
   # gamescope %command%
