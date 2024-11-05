78a81706687ffc3f90095b9b8c58a07a5a6675ee
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2024-11-05 12:45:17 +0100

    Get public key from file

--- home/bruno/features/cli/ssh.nix
+++ home/bruno/features/cli/ssh.nix
@@ -1,14 +1,7 @@
-{ pkgs, ... }:
+{...}: {
+  home.file.".ssh/id_ed25519_sk.pub".source = ../../ssh.pub;
 
-let
-  ssh-public-key = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBIkuC9ZoZYlmm6xb79mI1OCzh5cGLljSihrlIBzMwjkAAAABHNzaDo= Yubikey 5C";
-in {
-
-  home.file.".ssh/id_ed25519_sk.pub".text = 
-    "${ssh-public-key}";
-
-  home.file.".ssh/allowed_signers".text = 
-    "* ${ssh-public-key}";
+  home.file.".ssh/allowed_signers".text = "* ${builtins.readFile ../../ssh.pub}";
 
   programs.ssh = {
     enable = true;
