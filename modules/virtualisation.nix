6d158193657f0365d41d1f80f9c59d174c3a103d
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2024-11-29 09:34:14 +0100

    Disable libvirt and modify virtualbox to be able to run over KVM

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -8,8 +8,13 @@
   ];
 
   # Virtualbox
-  virtualisation.virtualbox.host.enable = true;
-  # virtualisation.virtualbox.host.enableExtensionPack = true; # Warning: This cause frequent recompilations
+  virtualisation.virtualbox.host = 
+  {
+    enable = true;
+    enableKvm = true;
+    addNetworkInterface = false;
+  };
+  
   users.extraGroups.vboxusers.members = [ "bruno" ];
   
   # Docker
@@ -17,23 +22,23 @@
   users.extraGroups.docker.members = [ "bruno" ];
 
   # Libvirt
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [(pkgs.OVMF.override {
-          secureBoot = true;
-          tpmSupport = true;
-        }).fd];
-      };
-    };
-  };
-  users.extraGroups.libvirtd.members = [ "bruno" ];
+  # virtualisation.libvirtd = {
+  #   enable = true;
+  #   qemu = {
+  #     package = pkgs.qemu_kvm;
+  #     runAsRoot = true;
+  #     swtpm.enable = true;
+  #     ovmf = {
+  #       enable = true;
+  #       packages = [(pkgs.OVMF.override {
+  #         secureBoot = true;
+  #         tpmSupport = true;
+  #       }).fd];
+  #     };
+  #   };
+  # };
+  # users.extraGroups.libvirtd.members = [ "bruno" ];
 
-  programs.virt-manager.enable = true;
+  # programs.virt-manager.enable = true;
 
 }
