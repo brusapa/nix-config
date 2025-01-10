ac93822d6b37a7dbe3282ab61adf48efc367e3c3
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-01-10 10:04:37 +0100

    Switch to brlaser package for printer

--- hosts/common/users/gurenda/default.nix
+++ hosts/common/users/gurenda/default.nix
@@ -1,12 +1,20 @@
-{ inputs, lib, config, pkgs, ... }:
-
 {
-
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  ifGroupExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+in {
   users.users.gurenda = {
     isNormalUser = true;
     description = "Gurenda";
     home = "/home/gurenda";
     createHome = true;
+    extraGroups = ifGroupExist [
+      "lp"
+      "scanner"
+    ];
   };
-
 }
