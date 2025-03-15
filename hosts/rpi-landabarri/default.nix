4b00687aae0b4f4f3ff66ca06549c3fb1c2f572b
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-03-15 14:01:32 +0100

    Share internet between two interfaces in rpi-landabarri

--- hosts/rpi-landabarri/default.nix
+++ hosts/rpi-landabarri/default.nix
@@ -7,6 +7,8 @@
     ../common/users/bruno
   ];
 
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
     initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
@@ -29,13 +31,59 @@
   networking.hostName = "rpi-landabarri";
   networking.useDHCP = lib.mkDefault true;
 
+  networking.interfaces.enp1s0u1u1 = {
+    useDHCP = false;
+    ipv4.addresses = [{
+      address = "192.168.0.1";
+      prefixLength = 24;
+    }];
+  };
+  networking.firewall.extraCommands = ''
+    # Set up SNAT on packets going from downstream to the wider internet
+    iptables -t nat -A POSTROUTING -o end0 -j MASQUERADE
+  '';
+  # Run a DHCP server on the downstream interface
+  services.kea.dhcp4 = {
+    enable = true;
+    settings = {
+      interfaces-config = {
+        interfaces = [
+          "enp1s0u1u1"
+        ];
+      };
+      lease-database = {
+        name = "/var/lib/kea/dhcp4.leases";
+        persist = true;
+        type = "memfile";
+      };
+      rebind-timer = 2000;
+      renew-timer = 1000;
+      subnet4 = [
+        {
+          id = 1;
+          pools = [
+            {
+              pool = "192.168.0.2 - 192.168.0.255";
+            }
+          ];
+          subnet = "192.168.0.1/24";
+        }
+      ];
+      valid-lifetime = 4000;
+      option-data = [{
+        name = "routers";
+        data = "192.168.0.1";
+      }];
+    };
+  };
+
    # Tailscale
   services.tailscale = {
     enable = true;
     openFirewall = true;
     useRoutingFeatures = "server";
     extraSetFlags = [
-      "--advertise-routes=10.80.10.0/24"
+      "--advertise-routes=10.80.10.0/24,192.168.0.0/24"
     ];
   };
 
