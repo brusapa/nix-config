da4155fdb6537ed20c5a221bbc3af3b7a74ab3ab
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-04-25 10:43:27 +0200

    Set merge option to merge

--- home/bruno/features/cli/git.nix
+++ home/bruno/features/cli/git.nix
@@ -12,6 +12,7 @@
       gpg.format = "ssh";
       gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
       user.signingkey = "~/.ssh/id_ed25519_sk.pub";
+      pull.rebase = false;
     };
   };
 
