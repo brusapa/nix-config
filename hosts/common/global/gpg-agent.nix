ee42a83bd5a2de10a815d6903e4f1b724ac7800d
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-05-09 12:53:23 +0200

    Switched pinentry-curses for pinentry-tty to check if works better with lazygit

--- hosts/common/global/gpg-agent.nix
+++ hosts/common/global/gpg-agent.nix
@@ -3,13 +3,14 @@
 
   environment.systemPackages = with pkgs; [
     pinentry-curses
+    pinentry-tty
   ];
 
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
     enableExtraSocket = true;
-    pinentryPackage = pkgs.pinentry-curses;
+    pinentryPackage = pkgs.pinentry-tty;
     settings = {
       default-cache-ttl = 60;
       max-cache-ttl = 120;
