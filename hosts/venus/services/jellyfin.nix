fd46f15179e6044e4ca12f053f6d3921fb286f50
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-05-11 19:15:53 +0200

    Add jellyfin with HW acceleration

--- hosts/venus/services/jellyfin.nix
+++ hosts/venus/services/jellyfin.nix
@@ -17,11 +17,37 @@
     device = "sun.brusapa.com:/mnt/Temporal/multimedia";
     fsType = "nfs";
   };
-  fileSystems."/mnt/plex" = {
-    device = "10.80.0.5:/volume2/plex";
-    fsType = "nfs";
-  };
   # optional, but ensures rpc-statsd is running for on demand mounting
   boot.supportedFilesystems = [ "nfs" ];
 
+  # Enable HW acceleration for Jellyfin
+  systemd.services.jellyfin.environment.LIBVA_DRIVER_NAME = "iHD";
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      libva-vdpau-driver
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      # OpenCL support for intel CPUs before 12th gen
+      # see: https://github.com/NixOS/nixpkgs/issues/356535
+      intel-compute-runtime-legacy1 
+      vpl-gpu-rt # QSV on 11th gen or newer
+      intel-ocl # OpenCL support
+    ];
+  };
+
+
+  services.jellyfin = {
+    enable = true;
+    user = "multimedia";
+    group = "multimedia";
+    openFirewall = true;
+  };
+
+  environment.systemPackages = [
+    pkgs.jellyfin
+    pkgs.jellyfin-web
+    pkgs.jellyfin-ffmpeg
+  ];
 }
