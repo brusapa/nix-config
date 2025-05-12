29e45bff96b9fc05f71bc396bcb8aa193f2ad242
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-05-12 07:43:17 +0200

    Switch main mars screen to 120hz

--- home/bruno/features/desktop/common/changeResolutionDesktopEntries.nix
+++ home/bruno/features/desktop/common/changeResolutionDesktopEntries.nix
@@ -4,14 +4,14 @@
   xdg.desktopEntries = {
     gaming-resolution = {
       name = "GamingResolution";
-      exec = "kscreen-doctor output.DP-3.mode.2560x1440@240";
+      exec = "kscreen-doctor output.DP-3.mode.2560x1440@120";
       terminal = false;
       type = "Application";
       categories = [ "Application" ];
     };
     native-resolution = {
       name = "NativeResolution";
-      exec = "kscreen-doctor output.DP-3.mode.5120x1440@240";
+      exec = "kscreen-doctor output.DP-3.mode.5120x1440@120";
       terminal = false;
       type = "Application";
       categories = [ "Application" ];
