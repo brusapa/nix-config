aa37605fd7161aaf3c13bc083bcd0348a0c4c359
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-05-25 20:52:59 +0200

    Use correct commit hash for caddy's cloudflare plugin

--- hosts/sun/services/caddy.nix
+++ hosts/sun/services/caddy.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.caddy = {
-  enable = true;
-  package = pkgs.caddy.withPlugins {
-    plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-    hash = "sha256-F/jqR4iEsklJFycTjSaW8B/V3iTGqqGOzwYBUXxRKrc=";
+    enable = true;
+    package = pkgs.caddy.withPlugins {
+      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
+      hash = "sha256-Gsuo+ripJSgKSYOM9/yl6Kt/6BFCA6BuTDvPdteinAI=";
+    };
   };
 }
