b4820a45de296c80f9f7752edc7ee1b21e556f6c
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-05-25 21:08:55 +0200

    Add caddy cloudflare config

--- hosts/sun/services/caddy.nix
+++ hosts/sun/services/caddy.nix
@@ -1,10 +1,38 @@
 { pkgs, ... }:
 {
+  # Import the needed secrets
+  sops = {
+    secrets = {
+      cloudflare-email = {
+        sopsFile = ../secrets.yaml;
+      };
+      cloudflare-api-token = {
+        sopsFile = ../secrets.yaml;
+      };
+    };
+    templates."caddy-secrets.env" = {
+      content = ''
+        CF_EMAIL="${config.sops.placeholder.cloudflare-email}"
+        CF_API_TOKEN="${config.sops.placeholder.cloudflare-api-token}"
+      '';
+      owner = "caddy";
+    };
+  };
+
+  systemd.services.caddy.serviceConfig.EnvironmentFile = config.sops.templates."caddy-secrets.env".path;
+
   services.caddy = {
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
       hash = "sha256-Gsuo+ripJSgKSYOM9/yl6Kt/6BFCA6BuTDvPdteinAI=";
     };
+    globalConfig = 
+    ''
+      {
+        email {env.CF_EMAIL}
+        acme_dns cloudflare {env.CF_API_TOKEN}
+      }
+    '';
   };
 }
