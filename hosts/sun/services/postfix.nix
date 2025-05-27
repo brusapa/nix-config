c129326ae2679f89941fe3d874743a37aaa9a01d
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-05-27 12:25:54 +0200

    Try to set root alias mail through virtual_alias_maps

--- hosts/sun/services/postfix.nix
+++ hosts/sun/services/postfix.nix
@@ -18,13 +18,18 @@
       '';
       owner = config.services.postfix.user;
     };
+    templates."root-alias" = {
+      content = ''
+        root: "${config.sops.placeholder.root-email-alias}"
+      '';
+      owner = config.services.postfix.user;
+    };
   };
 
   systemd.services.postfix.serviceConfig.EnvironmentFile = config.sops.templates."postfix-secrets.env".path;
 
   services.postfix = {
     enable = true;
-    rootAlias = "{env.ROOT_EMAIL_ALIAS}";
     relayHost = "smtp.eu.mailgun.org";
     relayPort = 587;
     config = {
@@ -32,6 +37,8 @@
       smtp_sasl_auth_enable = "yes";
       smtp_sasl_security_options = "";
       smtp_sasl_password_maps = "texthash:${config.sops.secrets."postfix/sasl_passwd".path}";
+      virtual_alias_maps = "hash:${config.sops.templates."root-alias".path}";
     };
+    aliasFiles."root-alias" = config.sops.templates."root-alias".path;
   };
 }
