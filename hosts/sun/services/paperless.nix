9c27daecff89f9c410410faca2f7d9e0d9033252
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-06-15 16:48:18 +0200

    Add paperless connection to gmail

--- hosts/sun/services/paperless.nix
+++ hosts/sun/services/paperless.nix
@@ -4,9 +4,21 @@
   # Import the needed secrets
   sops = {
     secrets = {
-      paperless-admin-password = {
+      "paperless/admin-password" = {
         sopsFile = ../secrets.yaml;
       };
+      "paperless/gmail-oauth-client-id" = {
+        sopsFile = ../secrets.yaml;
+      };
+      "paperless/gmail-oauth-client-secret" = {
+        sopsFile = ../secrets.yaml;
+      };
+    };
+    templates."paperless-secrets.env" = {
+      content = ''
+        PAPERLESS_GMAIL_OAUTH_CLIENT_ID=${config.sops.placeholder."paperless/gmail-oauth-client-id"};
+        PAPERLESS_GMAIL_OAUTH_CLIENT_SECRET=${config.sops.placeholder."paperless/gmail-oauth-client-secret"};
+      '';
     };
   };
 
@@ -17,7 +29,8 @@
       PAPERLESS_OCR_LANGUAGE = "spa+eus+eng";
       PAPERLESS_URL = "https://documentos.brusapa.com";
     };
-    passwordFile = config.sops.secrets.paperless-admin-password.path;
+    passwordFile = config.sops.secrets."paperless/admin-password".path;
+    environmentFile = config.sops.templates."paperless-secrets.env".path;
   };
   services.caddy.virtualHosts."documentos.brusapa.com".extraConfig = ''
     reverse_proxy http://127.0.0.1:28981
