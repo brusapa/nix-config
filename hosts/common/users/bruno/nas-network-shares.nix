65a52f82ec6c49830ec0e76b485f5a987e416a47
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-07-25 18:10:18 +0200

    Removed deprecated casa smb-share

--- hosts/common/users/bruno/nas-network-shares.nix
+++ hosts/common/users/bruno/nas-network-shares.nix
@@ -27,15 +27,4 @@
     in ["${automount_opts},credentials=${config.sops.templates."bruno-cifs-credentials".path},uid=${toString config.users.users.bruno.uid},gid=${toString config.users.groups.users.gid}"];
   };
 
-  # SMB NAS Casa Bruno
-  fileSystems."/home/bruno/NAS/casa" = {
-    device = "//sun.brusapa.com/casa";
-    fsType = "cifs";
-    options = let
-      # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-    in ["${automount_opts},credentials=${config.sops.templates."bruno-cifs-credentials".path},uid=${toString config.users.users.bruno.uid},gid=${toString config.users.groups.users.gid}"];
-  };
-
 }
--- hosts/common/users/gurenda/nas-network-shares.nix
+++ hosts/common/users/gurenda/nas-network-shares.nix
@@ -27,15 +27,4 @@
     in ["${automount_opts},credentials=${config.sops.templates."gurenda-cifs-credentials".path},uid=${toString config.users.users.gurenda.uid},gid=${toString config.users.groups.users.gid}"];
   };
 
-  # SMB NAS Casa Gurenda
-  fileSystems."/home/gurenda/NAS/casa" = {
-    device = "//sun.brusapa.com/casa";
-    fsType = "cifs";
-    options = let
-      # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-    in ["${automount_opts},credentials=${config.sops.templates."gurenda-cifs-credentials".path},uid=${toString config.users.users.gurenda.uid},gid=${toString config.users.groups.users.gid}"];
-  };
-
 }
