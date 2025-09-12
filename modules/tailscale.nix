4322708124e464ca098b6506344547b04c4e5ce7
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-09-12 10:02:42 +0200

    System upgrade and Tailscale fix for new kernel version

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -4,5 +4,20 @@
 
   services.tailscale.enable = true;
   services.tailscale.openFirewall = true;
+  nixpkgs.overlays = [
+    (_: prev: {
+      tailscale = prev.tailscale.overrideAttrs (old: {
+        checkFlags =
+          builtins.map (
+            flag:
+              if prev.lib.hasPrefix "-skip=" flag
+              then flag + "|^TestGetList$|^TestIgnoreLocallyBoundPorts$|^TestPoller$"
+              else flag
+          )
+          old.checkFlags;
+        });
+    })
+  ];
+
 
 }
