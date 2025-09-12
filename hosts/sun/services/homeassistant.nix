8f5c4eac14b8abd8b9569477a6c01a91838ec04b
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-09-12 10:04:55 +0200

    Add influxdb for home assistant data long term storage

--- hosts/sun/services/homeassistant.nix
+++ hosts/sun/services/homeassistant.nix
@@ -1,5 +1,23 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
+  # Import the needed secrets
+  sops = {
+    secrets = {
+      "influxdb/admin-password" = {
+        sopsFile = ../secrets.yaml;
+        owner = "influxdb2";
+      };
+      "influxdb/admin-token" = {
+        sopsFile = ../secrets.yaml;
+        owner = "influxdb2";
+      };
+      "influxdb/homeassistant-token" = {
+        sopsFile = ../secrets.yaml;
+        owner = "influxdb2";
+      };
+    };
+  };
+
   virtualisation.oci-containers = {
     backend = "podman";
 
@@ -10,7 +28,7 @@
       ];
       environment.TZ = "Europe/Madrid";
       # Note: The image will not be updated on rebuilds, unless the version label changes
-      image = "ghcr.io/home-assistant/home-assistant:2025.7";
+      image = "ghcr.io/home-assistant/home-assistant:2025.9";
       extraOptions = [ 
         # Use the host network namespace for all sockets
         "--network=host"
@@ -59,6 +77,43 @@
   services.glances = {
     enable = true;
   };
+
+  # EspHome 
+  services.esphome = {
+    enable = true;
+  };
+
+  # DB for historic data
+  services.influxdb2 = {
+    enable = true;
+    provision = {
+      enable = true;
+      initialSetup = {
+        organization = "main";
+        bucket = "main";
+        passwordFile = config.sops.secrets."influxdb/admin-password".path;
+        tokenFile = config.sops.secrets."influxdb/admin-token".path;
+      };
+      organizations = {
+        homeassistant = {
+          description = "Home assistant organization";
+          buckets.history = {
+            description = "Historic data from home assistant";
+            retention = 31536000; # 1 year
+          };
+          auths.homeassistant-token = {
+            writeBuckets = ["history"];
+            tokenFile = config.sops.secrets."influxdb/homeassistant-token".path;
+          };
+        };
+      };
+    };
+  };
+
+  services.caddy.virtualHosts."influxdb.brusapa.com".extraConfig = ''
+    reverse_proxy http://localhost:8086
+  '';
+
   services.caddy.virtualHosts."glances.brusapa.com".extraConfig = ''
     reverse_proxy http://127.0.0.1:61208
   '';
@@ -70,6 +125,10 @@
   '';
   services.caddy.virtualHosts."zigbee2mqtt-trastero.brusapa.com".extraConfig = ''
     reverse_proxy http://127.0.0.1:8082
+  '';
+
+  services.caddy.virtualHosts."esphome.brusapa.com".extraConfig = ''
+    reverse_proxy http://127.0.0.1:6052
   '';
 
   backup.job.home-assistant = {
