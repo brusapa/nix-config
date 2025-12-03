9db2fd54758cb4dceee6a869133b4305af103d22
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-12-03 09:15:08 +0100

    Make config more flake-parts-y

---
+++ flake/hosts.nix
@@ -0,0 +1,46 @@
+{ lib, ... }:
+
+{
+  options.hosts = lib.mkOption {
+    type = lib.types.attrsOf (lib.types.submodule ({ name, ... }: {
+      options = {
+        system = lib.mkOption {
+          type = lib.types.str;
+          default = "x86_64-linux";
+          description = "System type for host ${name}";
+        };
+
+        users = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          default = [];
+          description = "Home-manager users on host ${name}";
+        };
+      };
+    }));
+    default = {};
+    description = "Host definitions (system + home-manager users)";
+  };
+
+  config.hosts = {
+    sun = {
+      users = [ "bruno" ];
+    };
+
+    mars = {
+      users = [ "bruno" "gurenda" ];
+    };
+
+    mercury = {
+      users = [ "bruno" "gurenda" ];
+    };
+
+    wsl = {
+      users = [ "bruno" ];
+    };
+
+    rpi-landabarri = {
+      system = "aarch64-linux";
+      users = [ "bruno" ];
+    };
+  };
+}
