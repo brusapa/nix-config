ecbb281a44b3cd211f1071e03011d7be256c157b
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-12-05 10:03:39 +0100

    Removed deprecated libvirt option

--- modules/libvirtd.nix
+++ modules/libvirtd.nix
@@ -8,19 +8,12 @@
       package = pkgs.qemu_kvm;
       runAsRoot = true;
       swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [(pkgs.OVMF.override {
-          secureBoot = true;
-          tpmSupport = true;
-        }).fd];
-      };
       vhostUserPackages = [ pkgs.virtiofsd ];
     };
   };
 
   # Enable nested virtualization
-  boot.extraModprobeConfig = "options kvm_intel nested=1";
+  # boot.extraModprobeConfig = "options kvm_intel nested=1";
 
   programs.virt-manager.enable = true;
 
