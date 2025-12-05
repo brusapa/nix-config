bbf15ccb3fd4fda751a8a168c16bb480bffb017b
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2025-12-05 12:48:24 +0100

    Make RSSH asks for password

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -31,7 +31,12 @@
 
   # Allow sudo through ssh agent (RSSH)
   security.pam = {
-    rssh.enable = true;
+    rssh = {
+      enable = true;
+      settings = {
+        cue = true;
+      };
+    };
     services = {
       sshd.unixAuth = lib.mkForce true; # Specific users password login
       sudo.rssh = true; # Sudo over ssh
