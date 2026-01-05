28a7f24b924d904d267f0fcf75859f9cf3072f6b
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2026-01-05 07:51:46 +0100

    Add pluto to ssh connection

--- home/bruno/features/cli/ssh.nix
+++ home/bruno/features/cli/ssh.nix
@@ -15,6 +15,18 @@
         ];
         extraOptions.StreamLocalBindUnlink = "yes";
       };
+      "pluto" = {
+        hostname = "pluto.brusapa.com";
+        user = "bruno";
+        forwardAgent = true;
+        remoteForwards = [
+          {
+            bind.address = ''/run/user/1000/gnupg/S.gpg-agent'';
+            host.address = ''/run/user/1000/gnupg/S.gpg-agent.extra'';
+          }
+        ];
+        extraOptions.StreamLocalBindUnlink = "yes";
+      };
       "mars" = {
         hostname = "mars.brusapa.com";
         user = "bruno";
