c3269ecb3f70e82301a595afb93bbcc248eb5b82
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2026-06-07 18:44:58 +0200

    Add matter support on sun firewall

--- hosts/sun/services/homeassistant.nix
+++ hosts/sun/services/homeassistant.nix
@@ -105,6 +105,19 @@ in
       "primary-interface" = "iotVlan";
     };
   };
+  # Open mDNS on firewall for the matter server
+  networking.firewall = {
+    allowedUDPPorts = [ 5353 ];
+    # Crucial for mDNS / Multicast on Linux firewalls
+    extraCommands = ''
+      # Allow incoming multicast traffic for mDNS
+      iptables -A INPUT -d 224.0.0.251 -p udp --dport 5353 -j ACCEPT
+      ip6tables -A INPUT -d ff02::fb -p udp --dport 5353 -j ACCEPT
+
+      # Allow Matter link-local multicast
+      ip6tables -A INPUT -d ff02::1 -p udp -j ACCEPT
+    '';
+  };
 
   # Glances for homeassistant monitoring of the server
   services.glances = {
