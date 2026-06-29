e004b4a3c167a164bfefbd5406edffec45d59828
Bruno Santamaria <30648587+brusapa@users.noreply.github.com>
2026-06-29 08:23:37 +0200

    Do not decrypt pluto pool on boot

--- hosts/pluto/services/sun-backup.nix
+++ hosts/pluto/services/sun-backup.nix
@@ -3,19 +3,6 @@ let
   # Source variables
   sourcePool = "zfspuller@sun.brusapa.com:zstorage";
   datasets = [ "users" "paperless" "photos" "internal-backups" ];
-
-  syncoidCommonArgs = [
-    # Tell syncoid not to create its own snapshots; rely on sanoid
-    "--no-sync-snap"
-    # This argument tells syncoid to create a zfs bookmark for the newest snapshot after it got replicated successfully.
-    "--create-bookmark"
-    # Prune old snapshots on the target that don't exist on the source
-    "--delete-target-snapshots"
-    # Do not mount newly received datasets
-    "--recvoptions=-u"
-    # Use raw. End-to-end encrypted backups
-    "--sendoptions=-w"
-  ];
 in {
 
   # Import the needed secrets
@@ -34,7 +21,6 @@ in {
     enable = true;
     interval = "hourly";
     sshKey = config.sops.secrets.sun-zfspuller-private-key.path;
-    commonArgs = syncoidCommonArgs;
     localTargetAllow = [
       "change-key"
       "compression"
@@ -51,6 +37,16 @@ in {
       value = {
         source = "${sourcePool}/${dataset}";
         target = "zbackup/${dataset}";
+        sendOptions="w";
+        recvOptions="u";
+        extraArgs = [
+          # Tell syncoid not to create its own snapshots; rely on sanoid
+          "--no-sync-snap"
+          # This argument tells syncoid to create a zfs bookmark for the newest snapshot after it got replicated successfully.
+          "--create-bookmark"
+          # Prune old snapshots on the target that don't exist on the source
+          "--delete-target-snapshots"
+        ];
       };
     }) datasets);
   };
